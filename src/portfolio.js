205c99e211d0ced252361659d9c1456d01a23db7
chriscaira <chriscaira@nousot.com>
2024-12-09 14:49:35 -0600

    changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://ChrisCaira2.github.io',
+  title: 'CC.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Chris Caira',
+  role: 'Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Looking for Entry Level Software Engineer opportunites or Internships',
+  resume: 'https://drive.google.com/file/d/1GZlnwQA8lGjqRw_hoXCK32vWceUzFJcu/view',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/chris-caira-2ba4a31a9/',
+    github: 'https://github.com/ChrisCaira2',
   },
 }
 
@@ -21,51 +21,52 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Addy',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A React Native app that allows users to find events to host or find people to attend their events with a swipe interface.',
+    stack: ['React Native', 'Javascript', 'Firebase'],
   },
   {
-    name: 'Project 2',
+    name: 'Bet Track',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Web application that pulls in betting odds for NBA, MLB, NFL games and allows user to track their bets for the day.',
+    stack: ['Javascript', 'React', 'MongoDB'],
+    sourceCode: 'https://github.com/ChrisCaira2/BetTrack',
   },
   {
-    name: 'Project 3',
+    name: 'Employee Rolodex',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Internship project where myself and a team of interns created a company rolodex for their internal site. Employees could create a profil and view their coworkers profiles.',
+    stack: ['React', 'Javascript', 'MongoDB'],
+  },
+  {
+    name: 'Automated Finance Sheet',
+    description:
+      'Internship project where myself and a team of interns created an automated finance sheet which pulled directly from an API with all expenses for the company and formatted the companies budget into a Google Sheet.',
+    stack: ['Python'],
+  },
+  {
+    name: 'Infinite Runner',
+    description:
+      'Arcade game made using Python and Pygame, user tries to avoid obstacles as they progressively get faster.',
+    stack: ['Python', 'Pygame'],
+    sourceCode: 'https://github.com/ChrisCaira2/InfiniteRunner',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
+  'Python',
+  'Java',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'SQL',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'ccaira1401@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
